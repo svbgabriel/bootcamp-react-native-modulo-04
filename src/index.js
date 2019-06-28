a94d0b4d06fa7f32dc1aa644519e2da205b4d576
Gabriel Batista <svbgabriel@gmail.com>
2019-06-27 21:55:53 -0300

    Estiliza o Player

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import '~/config/StatusBarConfig';
 import React from 'react';
 import { Provider } from 'react-redux';
 import Routes from '~/routes';
-
 import store from '~/store';
+import Player from '~/components/Player';
 
 const App = () => (
   <Provider store={store}>
     <Routes />
+    <Player />
   </Provider>
 );
 
